ebe45a7c35f0d6683f0bffca0fbef867ea762a03
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-04-19 19:15:14 +0430

    JS data types

--- 04_Javascript/01_Introduction_to_javascript.js
+++ 04_Javascript/01_Introduction_to_javascript.js
@@ -10,3 +10,18 @@ let x;
 // Values can be assigned to variables with an = sign
 x = 0;
 console.log(x)
+
+// JavaScript supports several types of values
+y = 1   // Integer numbers
+console.log(y)
+z = 0.01    // Real numbers
+console.log(z)
+s_1 = 'single quote string'     // String of text in single quotation marks
+s_2 = "double quote string"     // String of text in double quotation marks
+console.log(s_1, ", ", s_2)
+b_1 = true      // A boolean value
+b_2 = false     // Another boolean value
+console.log(b_1, ", ", b_2)
+n = null    // Null is a special value that means "no value."
+u = undefined   // Undefined is another special value like null.
+console.log(n, ", ", u)
