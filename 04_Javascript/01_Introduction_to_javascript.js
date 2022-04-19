c440f2963eafa4b3d0aebb799791e8e7a04504eb
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-04-19 18:22:10 +0430

    Declaring a variable and assigning a value to it

--- 04_Javascript/01_Introduction_to_javascript.js
+++ 04_Javascript/01_Introduction_to_javascript.js
@@ -1,2 +1,12 @@
 // Simple hello world in js
 console.log("Hello World!")
+
+// Anything following double slashes is an English-language comment.
+
+// A variable is a symbolic name for a value.
+// Variables are declared with the let keyword:
+// Declare a variable named x
+let x;
+// Values can be assigned to variables with an = sign
+x = 0;
+console.log(x)
