92e1656367599f0b64477a2e34e31f97701a90d0
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-04-22 03:47:14 -0700

    Make console.log better

--- 04_Javascript/01_Introduction_to_javascript.js
+++ 04_Javascript/01_Introduction_to_javascript.js
@@ -9,22 +9,22 @@ console.log("Hello World!")
 let x;
 // Values can be assigned to variables with an = sign
 x = 0;
-console.log(x)
+console.log("x:", x)
 
 // JavaScript supports several types of values
 y = 1   // Integer numbers
-console.log(y)
+console.log("y:", y)
 z = 0.01    // Real numbers
-console.log(z)
+console.log("z:", z)
 s_1 = 'single quote string'     // String of text in single quotation marks
 s_2 = "double quote string"     // String of text in double quotation marks
-console.log(s_1, ", ", s_2)
+console.log("s_1:",s_1, ", s_2:", s_2)
 b_1 = true      // A boolean value
 b_2 = false     // Another boolean value
-console.log(b_1, ", ", b_2)
+console.log("b_1:", b_1, ", b_2:", b_2)
 n = null    // Null is a special value that means "no value."
 u = undefined   // Undefined is another special value like null.
-console.log(n, ", ", u)
+console.log("n:", n, ", u:", u)
 
 // JavaScript's most important datatype is the object.
 // An object is a collection of name/value pairs, or a string to value map.
@@ -32,4 +32,4 @@ let book = {
     topic: "javascript",
     edition: 7
 }
-console.log(book)
+console.log("book:", book)
