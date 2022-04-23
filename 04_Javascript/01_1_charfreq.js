855d6321e37fbf7a85993051be4cc6b80d169dcc
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-04-23 15:11:31 +0430

    Defining and implementing DefaultMap class

--- 04_Javascript/01_1_charfreq.js
+++ 04_Javascript/01_1_charfreq.js
@@ -6,3 +6,22 @@
  * In a Unix-type environment you can invoke the program like this:
  * node charfreq.js < corpus.txt
  */
+
+
+// This class extends Map so that the get() method returns the specified
+// value instead of null when the key is not in the map
+class DefaultMap extends Map{
+    constructor(defaultValue) {
+        super();
+        this.defaultValue = defaultValue;
+    }
+
+    get(key){
+        if (this.has(key)){
+            return super.get(key);
+        }
+        else {
+            return this.defaultValue;
+        }
+    }
+}
