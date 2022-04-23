723a72425515815bb5810ef764387c7c27cf0ae4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-04-23 15:23:54 +0430

    Defining and implementing Histogram class

--- 04_Javascript/01_1_charfreq.js
+++ 04_Javascript/01_1_charfreq.js
@@ -25,3 +25,39 @@ class DefaultMap extends Map{
         }
     }
 }
+
+class Histogram {
+    constructor() {
+        this.letterCounts = new DefaultMap(0);
+        this.totalLetters = 0;
+    }
+
+    add(text) {
+        text = text.replace(/\s/g, "").toUpperCase();
+        for (let character of text){
+            let count = this.letterCounts.get(character);
+            this.letterCounts.set(character, count+1);
+            this.totalLetters++;
+        }
+    }
+
+    toString(){
+        let entries = [...this.letterCounts];
+        entries.sort((a, b) => {
+            if (a[1] === b[1]){
+                return a[0] < b[0] ? -1 : 1;
+            }
+            else {
+                return b[1] - a[1];
+            }
+            });
+        for (let entry of entries){
+            entry[1] = entry[1] / this.totalLetters * 100;
+        }
+        entries = entries.filter(entry => entry[1] >= 1);
+        let lines = entries.map(
+            ([l,n]) => `${l}: ${"#".repeat(Math.round(n))} ${n.toFixed(2)}%`
+        )
+        return lines.join("\n")
+    }
+}
