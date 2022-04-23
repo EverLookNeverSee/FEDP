7bf66330ce94d6abc91d1ac5b3d2185ef8069a8b
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-04-23 14:12:35 +0430

    A function to compute factorials using for loop

--- 04_Javascript/01_Introduction_to_javascript.js
+++ 04_Javascript/01_Introduction_to_javascript.js
@@ -190,3 +190,13 @@ function factorial(n) {
     return product
 }
 console.log("factorial(5) -->", factorial(5))
+
+// A function to compute factorials using for loop
+function factorial_2(n) {
+    let i, product = 1
+    for (i=2; i <= n; i++){
+        product *= i
+    }
+    return product
+}
+console.log("factorial_2(5) -->", factorial_2(5))
