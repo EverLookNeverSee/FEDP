806c22188a1743f266127209facc1d7c879295d5
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-04-24 11:34:01 +0430

    Adding initial comments

---
+++ 04_Javascript/02_Lexical_Structure.js
@@ -0,0 +1,8 @@
+// This is a single line comment.
+
+/* This is also a comment*/     // and here is another comment
+
+/*
+* This is a multi-line comment. The extra * character at the start of
+* each line are not e required part of the syntax. they just look cool.
+*/
