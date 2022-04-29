43281e3096fb1893516e2363ffe4de28e3e491ee
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-04-29 12:07:49 +0430

    Adding Math object methods

--- 04_Javascript/03_Types_Values_Variables.js
+++ 04_Javascript/03_Types_Values_Variables.js
@@ -39,3 +39,42 @@ let bits = 0b0001_1101_0111;            // As a nibble separator
 console.log("bits:", bits)
 let fraction = 0.123_456_789;           // Works for fractional part, too.
 console.log("fraction:", fraction)
+
+
+// Arithmetic in Javascript:
+// + --> Addition , - --> Subtraction , * --> Multiplication
+// / --> Division , % --> Modulo , ** --> Exponentiation
+
+// Math Object:
+console.log("Math.pow(2, 53):", Math.pow(2, 53))        // 2 to the power 53
+console.log("Math.round(.4):", Math.round(.4))      // round to the nearest integer
+console.log("Math.round(.6):", Math.round(.6))
+console.log("Math.ceil(.4):", Math.ceil(.4))        // round up to an integer
+console.log("Math.floor(.6):", Math.floor(.6))      // round down to an integer
+console.log("Math.abs(-5):", Math.abs(-5))      // absolute value
+console.log("Math.max(1, 5, 8):", Math.max(1, 5, 8))        // return the largest argument
+console.log("Math.min(2, 4, 9):", Math.min(2, 4, 9))        // return the smallest argument
+console.log("Math.random():", Math.random())        // pseudo-random number 0 =< x < 1
+console.log("Math.PI:", Math.PI)        // 3.1415...
+console.log("Math.E:", Math.E)      // the base of the natural logarithm
+console.log("Math.sqrt(3):", Math.sqrt(3))      // the square root of 3
+console.log("Math.pow(3, 1/3):", Math.pow(3, 1 / 3))        // the cube root of 3
+console.log("Math.sin(0):", Math.sin(0))        // sin(x)
+console.log("Math.log(10):", Math.log(10))      // natural logarithm of 10
+console.log("Math.log(100) / Math.LN10:", Math.log(100) / Math.LN10)        // base 10 logarithm of 100
+console.log("Math.log(512) / Math.LN2:", Math.log(512) / Math.LN2)        // base 2 logarithm of 512
+console.log("Math.cbrt(27):", Math.cbrt(27))        // cube root
+console.log("Math.hypot(3, 4):", Math.hypot(3, 4))  // square root of sum of squares of all arguments
+console.log("Math.log10(100):", Math.log10(100))        // base-10 logarithm
+console.log("Math.log2(1024):", Math.log2(1024))        // base-2 logarithm
+console.log("Math.lo1p(.0002):", Math.log1p(.0002))   // natural log of (1+x); accurate for very small x
+console.log("Math.expm1(2):", Math.expm1(2))          // Math.exp(x) - 1
+console.log("Math.sign(5):", Math.sign(5))      // -1, 0, 1 for arguments <, == or > 0
+console.log("Math.sign(0):", Math.sign(0))
+console.log("Math.sign(-4):", Math.sign(-4))
+console.log("Math.imul(2, 3):", Math.imul(2, 3))    // optimized multiplication of 32-bit integers
+console.log("Math.clz32(0xf):", Math.clz32(0xf))        // number of leading zero bits in 32-bit integer
+console.log("Math.trunc(3.9):", Math.trunc(3.9))        // truncating fractional part
+console.log("Math.fround(4.66):", Math.fround(4.66))      // round to the nearest 32-bit float
+console.log("Math.sinh(0):", Math.sinh(0))      // Hyperbolic sine
+console.log("Math.asinh(0)", Math.asinh(0))     // Hyperbolic arcsine
