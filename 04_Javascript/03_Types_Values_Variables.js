c51adbb02e4ec3ec484d6034ebc168f2451e46e4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-04-29 10:20:42 +0430

    Adding numbers section initial descriptions

--- 04_Javascript/03_Types_Values_Variables.js
+++ 04_Javascript/03_Types_Values_Variables.js
@@ -2,3 +2,11 @@ let a = [1, 4, 2, 5, 3, 6];
 console.log("a:", a)
 let s_a = a.sort();     // OOP Version
 console.log("sorted a:", s_a)
+
+
+// Numbers:
+// JavaScript represents numbers using the 64-bit floating-point format defined by the IEEE 754 standard,
+// which means it can represent numbers as large as ±1.7976931348623157 × 10 308 and as small as
+// ±5 × 10 −324 .
+// The JavaScript number format allows you to exactly represent all integers between
+// −9,007,199,254,740,992 (−2 53 ) and 9,007,199,254,740,992 (2 53 ), inclusive.
