a198e50bae226a5f865a192e662fe09fd37e72aa
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-01 22:49:19 +0430

    Escape Sequences in String Literals

--- 04_Javascript/03_Types_Values_Variables.js
+++ 04_Javascript/03_Types_Values_Variables.js
@@ -218,3 +218,24 @@ is included literally in this string`)
 // within a JavaScript expression, which is then double-quoted within an HTML
 // event-handler attribute:
 // <button onclick="alert('Thank you')">Click Me</button>
+
+
+// Escape Sequences in String Literals:
+let sample_01 = 'you \'re right. it can\'t be a quote.'
+console.log(sample_01)
+let sample_02 = "\u03c0"        // pi symbol
+console.log(sample_02)
+
+// \0 The NUL character ( \u0000 )
+// \b Backspace ( \u0008 )
+// \t Horizontal tab ( \u0009 )
+// \n Newline ( \u000A )
+// \v Vertical tab ( \u000B )
+// \f Form feed ( \u000C )
+// \r Carriage return ( \u000D )
+// \" Double quote ( \u0022 )
+// \' Apostrophe or single quote ( \u0027 )
+// \\ Backslash ( \u005C )
+// \x nn The Unicode character specified by the two hexadecimal digits nn
+// \u nnnn The Unicode character specified by the four hexadecimal digits nnnn
+// \u{ n } The Unicode character specified by the codepoint n, where n is one to six hexadecimal digits between 0 and 10FFFF (ES6)
